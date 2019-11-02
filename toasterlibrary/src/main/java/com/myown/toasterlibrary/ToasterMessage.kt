a06b0a89fae35963377e0ec1a760869ce91ce751
carlosvimos <medialeaders2020>
2019-11-02 22:52:37 +0100

    replaced

--- toasterlibrary/src/main/java/com/myown/toasterlibrary/ToasterMessage.kt
+++ toasterlibrary/src/main/java/com/myown/toasterlibrary/ToasterMessage.kt
@@ -3,10 +3,7 @@ package com.myown.toasterlibrary
 import android.content.Context
 import android.widget.Toast
 
-class ToasterMessage {
-
-    fun Context.s(msg: String) {
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
-    }
+fun Context.showToast(msg: String) {
+    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
 
 }
